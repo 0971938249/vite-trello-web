e8f8886a23f98a1da5a15b6f7b43db8bd9a8c02d
Nhật Quang <donhatquang010102@gmail.com>
2024-09-20 13:44:29 +0700

    config_mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Button from '@mui/material/Button'
-
+import  Typography  from '@mui/material/Typography'
 function App() {
 
   return (
     <>
       <div>Nhật Quang</div>
-      <Button variant="contained">Hello world</Button>
+      <Typography variant='body2'color="secondary">ádadasda</Typography>
+      <Button variant="contained" color="secondary">Hello world</Button>
       <AccessAlarmIcon/>
       <ThreeDRotation/>
     </>
