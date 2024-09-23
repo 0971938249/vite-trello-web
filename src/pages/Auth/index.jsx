d527c1d0739362acb886122f8ef3a3620342782b
Nhật Quang <donhatquang010102@gmail.com>
2024-09-23 22:58:50 +0700

    refactor_project_structure

---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,1 @@
+//Authentication/ sign up
---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,18 @@
+//Boards list
+import Container from '@mui/material/Container'
+import AppBar from '../../components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+function BoardDetails() {
+  return (
+    <Container maxWidth={false } disableGutters sx={{ height:'100vh' }}>
+      <AppBar/>
+      <BoardBar/>
+      <BoardContent/>
+    </Container>
+  )
+}
+
+export default BoardDetails
+
